8d185772334a0e4376762b3e9e11de8ab18de294
MJ <mjcardilino@gmail.com>
2017-04-28 18:10:23 -0400

    adding in jersey boy's matlab code

--- videoLog.m
+++ videoLog.m
@@ -1,4 +1,6 @@
 clear; close all; clc
+% Add the path to the HMM gesture recognition toolkit
+addpath('gesture', 'gesture/data/test', 'gesture/data/train');
 
 % Color data
 vid_color = videoinput('kinect', 1);
