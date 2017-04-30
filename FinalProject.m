3d26f53622deb6288ed1e6bc3cd9f1fcd924fb8a
Christopher Lane McClure <cmcclure17@gmail.com>
2017-04-30 15:49:59 -0400

    we got some data

--- FinalProject.m
+++ FinalProject.m
@@ -1,12 +1,12 @@
 clear; close all; clc
 imaqreset;
 
-% load('TrainingSets.mat');
+load('TrainingSets.mat');
 
 saveDataForTrainingSets = true;
 
-test_gesture = 'somegesture';
-train_gestures = {'circle' 'z' 'l' 'm' 'round' 'x'};
+test_gesture = 'noisy_cw';
+train_gestures = {'circle' 'z' 'l' 'm' 'round' 'x' 'swipe_right'};
 
 % Add the path to the HMM gesture recognition toolkit
 addpath('gesture', 'gesture/data/test', 'gesture/data/train');
@@ -15,15 +15,62 @@ dynamicDataCollection;
 
 if saveDataForTrainingSets
     switch(test_gesture)
-        case 'somegesture'
+        case 'swipe_right'
             if exist(test_gesture)
-                [~, numSets, ~, ~] = size(somegesture);
-                somegesture = positionData;
-                somegesture(:,numSets+1,:,:) = positionData;
+                [~, numSets, ~, ~] = size(swipe_right);
+                swipe_right = positionData;
+                swipe_right(:,numSets+1,:,:) = positionData;
             else
-                somegesture = positionData;
+                swipe_right = positionData;
+            end
+            
+        case 'noisy_right'
+            if exist(test_gesture)
+                [~, numSets, ~, ~] = size(noisy_right);
+                noisy_right = positionData;
+                noisy_right(:,numSets+1,:,:) = positionData;
+            else
+                noisy_right = positionData;
+            end
+            
+        case 'cw_circle'
+            if exist(test_gesture)
+                [~, numSets, ~, ~] = size(cw_circle);
+                cw_circle = positionData;
+                cw_circle(:,numSets+1,:,:) = positionData;
+            else
+                cw_circle = positionData;
+            end
+            
+        case 'noisy_cw'
+            if exist(test_gesture)
+                [~, numSets, ~, ~] = size(noisy_cw);
+                noisy_cw = positionData;
+                noisy_cw(:,numSets+1,:,:) = positionData;
+            else
+                noisy_cw = positionData;
+            end
+            
+        case 'ccw_circle'
+            if exist(test_gesture)
+                [~, numSets, ~, ~] = size(ccw_circle);
+                ccw_circle = positionData;
+                ccw_circle(:,numSets+1,:,:) = positionData;
+            else
+                ccw_circle = positionData;
+            end
+            
+        case 'swipe_left'
+            if exist(test_gesture)
+                [~, numSets, ~, ~] = size(swipe_left);
+                swipe_left = positionData;
+                swipe_left(:,numSets+1,:,:) = positionData;
+            else
+                swipe_left = positionData;
             end
     end
+    
+    save('TrainingSets.mat', 'swipe_right', 'cw_circle', 'ccw_circle', 'swipe_left', 'noisy_right', 'noisy_cw');
 else
     for trained_sets = 1:length(train_gestures)
         if trained_sets <= 6
--- dynamicDataCollection.m
+++ dynamicDataCollection.m
@@ -1,5 +1,5 @@
 numTriggers = 1;
-framesPerTrigger = 5;
+framesPerTrigger = 100;
 
 % Depth data
 vid_depth = videoinput('kinect', 2);
@@ -8,14 +8,22 @@ src_depth.EnableBodyTracking = 'on';
 vid_depth.FramesPerTrigger = framesPerTrigger;
 % triggerconfig([vid_color vid_depth], 'manual');
 triggerconfig(vid_depth, 'manual');
-for ctr = 1:numTriggers
+ctr = 1;
+triggered = 0;
+while triggered < 2
     fprintf(['run ' num2str(ctr) ' complete']);
     start(vid_depth);
     trigger(vid_depth);
     
     [frame, ts, metaData] = getdata(vid_depth);
-    for frame = 1:vid_depth.FramesPerTrigger
-        positionData(:,:,(ctr-1)*5+frame) = metaData(frame).JointPositions(:,:,1);
+    trackedBody = find(metaData(1).IsBodyTracked == 1);
+    if ~isempty(trackedBody)
+        triggered = triggered + 1
+    end
+    if triggered == 2
+        for frame = 1:vid_depth.FramesPerTrigger
+            positionData(:,:,(ctr-1)*5+frame) = metaData(frame).JointPositions(:,:,trackedBody(1));
+        end
     end
 end
 stop(vid_depth)
